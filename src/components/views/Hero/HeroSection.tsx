e2599037ced038ebe2fda4aa80c14c605b1e923f
Muhammad Rafi Ali <therafiali@gmail.com>
2025-12-13 16:22:21 +0500

    format

--- src/components/views/Hero/HeroSection.tsx
+++ src/components/views/Hero/HeroSection.tsx
@@ -1,52 +1,46 @@
-
-
-
-
 const HeroSection = () => {
-
-    return <>
-        <section className="hero relative w-full min-h-[50vh] lg:h-[822px] text-white Right">
-            {/* bg image */}
-            <div className="absolute left-0 top-0 z-[-1] h-full w-full">
-                <img
-                    alt="Let patients shop between pharmacies."
-                    width={2880}
-                    height={1600}
-                    decoding="async"
-                    data-nimg={1}
-                    className="h-full w-full object-cover object-bottom"
-                    sizes="100vw"
-                 
-                    src="/bg.webp"
-                    style={{ color: "transparent" }}
-                />
-            </div>
-            <div className="l-container l-container--hero__content relative z-0 flex flex-col-reverse pb-[40px] lg:flex-row lg:items-end min-h-[50vh] lg:h-[822px] justify-center lg:justify-start">
-                {/* main headphoone image */}
-                <div className="absolute inset-0 flex items-center justify-center text-center">
-                    <span className="text-6xl lg:text-7xl font-black uppercase tracking-tight opacity-90 leading-none">
-                        Immersive studio sound
-                    </span>
-                </div>
-                <div className="hero-center-image pointer-events-none absolute inset-0 z-[10] hidden h-full w-full items-center justify-center lg:flex">
-                    <img
-                        alt="Let patients shop between pharmacies."
-                        width={2000}
-                        height={2000}
-                        decoding="async"
-                        data-nimg={1}
-                        className="mx-auto h-auto w-[48vw]  min-w-[1200px]"
-                        sizes="(max-width: 800px) 100vw, 500px"
-                        srcSet='/heroimage.jpg'
-                        src='/heroimage.jpg'
-                        style={{ color: "transparent" }}
-                    />
-                </div>
-
-            </div>
-        </section>
-
+  return (
+    <>
+      <section className="hero relative w-full min-h-[50vh] lg:h-[822px] text-white Right">
+        {/* bg image */}
+        <div className="absolute left-0 top-0 z-[-1] h-full w-full">
+          <img
+            alt="Let patients shop between pharmacies."
+            width={2880}
+            height={1600}
+            decoding="async"
+            data-nimg={1}
+            className="h-full w-full object-cover object-bottom"
+            sizes="100vw"
+            src="/bg.webp"
+            style={{ color: "transparent" }}
+          />
+        </div>
+        <div className="l-container l-container--hero__content relative z-0 flex flex-col-reverse pb-[40px] lg:flex-row lg:items-end min-h-[50vh] lg:h-[822px] justify-center lg:justify-start">
+          {/* main headphoone image */}
+          <div className="absolute inset-0 flex items-center justify-center text-center">
+            <span className="text-6xl lg:text-7xl font-black uppercase tracking-tight opacity-90 leading-none">
+              Immersive studio sound
+            </span>
+          </div>
+          <div className="hero-center-image pointer-events-none absolute inset-0 z-[10] hidden h-full w-full items-center justify-center lg:flex">
+            <img
+              alt="Let patients shop between pharmacies."
+              width={2000}
+              height={2000}
+              decoding="async"
+              data-nimg={1}
+              className="mx-auto h-auto w-[48vw]  min-w-[1200px]"
+              sizes="(max-width: 800px) 100vw, 500px"
+              srcSet="/heroimage.jpg"
+              src="/heroimage.jpg"
+              style={{ color: "transparent" }}
+            />
+          </div>
+        </div>
+      </section>
     </>
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
